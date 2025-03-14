d6e7094762109aaf8dbd4aa9f474761658d8aec8
M-ajb <195341850+M-ajb@users.noreply.github.com>
2025-03-14 01:20:09 +0100

    feat: Implemented Employee REST API and extended service methods
    - Added `@GetMapping`, `@PostMapping`, `@PutMapping`, and `@DeleteMapping` in `EmployeeRestController` for full CRUD functionality.
    - Extended methods in `EmployeeServiceImpl` to implement business logic.
    - Created package structure for `entity`, `dao`, `service`, and `rest`.
    - Implemented `EmployeeRepository`, `EmployeeService`, `EmployeeServiceImpl`, and `EmployeeRestController`.
    
    Completed the foundational structure and CRUD operations for Employee management.

--- src/main/java/com/portfolio/hr_system/service/EmployeeServiceImpl.java
+++ src/main/java/com/portfolio/hr_system/service/EmployeeServiceImpl.java
@@ -1,29 +1,47 @@
 package com.portfolio.hr_system.service;
 
+import com.portfolio.hr_system.dao.EmployeeRepository;
 import com.portfolio.hr_system.entity.Employee;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
+
+    private EmployeeRepository employeeRepository;;
+
+    public EmployeeServiceImpl(EmployeeRepository theEmployeeRepository) {
+        employeeRepository = theEmployeeRepository;
+    }
+
     @Override
     public List<Employee> findAll() {
-        return List.of();
+        return employeeRepository.findAll();
     }
 
     @Override
     public Employee findById(int theId) {
-        return null;
+        Optional<Employee> result = employeeRepository.findById(theId);
+        Employee theEmployee = null;
+
+        if(result.isPresent()){
+            theEmployee = result.get();
+        }else{
+            throw new RuntimeException("Did not find employee id - " + theId);
+        }
+        return theEmployee;
     }
 
     @Override
     public Employee save(Employee theEmployee) {
-        return null;
+        return employeeRepository.save(theEmployee);
     }
 
     @Override
     public void deleteById(int theId) {
+        employeeRepository.deleteById(theId);
 
     }
 }
