65630bf39edef5861eb77f2395339e6ed0cb8100
M-ajb <195341850+M-ajb@users.noreply.github.com>
2025-03-18 17:23:10 +0100

    feat: Migrated from REST API to MVC with Thymeleaf
    - Removed `rest` package and `EmployeeRestController` class.
    - Created `controller` package and added `EmployeeController` with `@Controller` annotation.
    - Implemented `/employees/list` endpoint to return Thymeleaf template.
    - Added `list-employees.html` for rendering employee data.
    - Included `spring-boot-starter-thymeleaf` dependency for Thymeleaf support.
    
    Switched from REST API to MVC architecture using Thymeleaf templates.

---
+++ src/main/java/com/portfolio/hr_system/controller/EmployeeController.java
@@ -0,0 +1,36 @@
+package com.portfolio.hr_system.controller;
+
+import com.portfolio.hr_system.entity.Employee;
+import com.portfolio.hr_system.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+
+    @GetMapping("/list")
+    public String listEmployees(Model theModel) {
+
+        // get employees from db
+        List<Employee> theEmployees = employeeService.findAll();
+
+        // add to the spring model
+        theModel.addAttribute("employees", theEmployees);
+
+        return "list-employees";
+    }
+}
